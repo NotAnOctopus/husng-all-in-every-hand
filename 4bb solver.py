1d225c0f075b73bd33ec8b55a74e26e8f2e32438
NotAnOctopus <zapmeister999@hotmail.co.uk>
2016-06-11 20:52:54 +0100

    made randomiser faster and combined shove lists

--- 4bb solver.py
+++ 4bb solver.py
@@ -1,21 +1,21 @@
+import numpy as numpy
+from scipy.sparse import linalg
 import random
 from equities import rankedcards, equities, f, g, h
-import numpy as numpy
-from scipy.sparse import linalg # this. this sparse linalg solver is the only reason i installed numpy and scipy. and it took me two bloody hours to find out how to install the damn things. why does it have to be so complicated? and what the hell is pip and why do i have to upgrade it every five nanoseconds?
+# this. this sparse linalg solver is the only reason i installed numpy and scipy. and it took me two bloody hours to find out how to install the damn things. why does it have to be so complicated? and what the hell is pip and why do i have to upgrade it every five nanoseconds?
 
 # this thing is a calculator to work out optimal shove ranges against someone going all in every hand in a poker heads up sit and go, where you know that they're going all in every hand.
 # yeah some people genuinely really do this.
 # this is a toy version where blinds are 1/2 and total start stacks are 8
-# j=shovesb[n] means if you have a stack size of n in the sb, you shove top j+1 out of 169 hands ranked in order (i.e. top hand is aces and bottom hand is 32off)
-# same with shovebb
+# j=shove[n] means if your stack size and position are indicated by n in the array below, you shove top j+1 out of 169 hands ranked in order (i.e. top hand is aces and bottom hand is 32off)
 
-shovesb=[168, 168, 160, 150, 140, 150, 160] # this is a starting point; these ranges are way off
-shovebb=[168, 168, 168, 150, 140, 150] # so are these
+# pos: bb1  sb1  bb2  sb2  bb3  sb3  bb4  sb4  bb5  sb5  bb6  sb6
+shove=[168, 168, 168, 160, 150, 150, 140, 140, 150, 160, 168, 160] # these ranges are way too tight; it's a starting point
 
 fish=0.0
 
-def yourface(shovesb, shovebb):
-    [aa,bb,cc,dd,ee,ff,gg,hh,ii,jj,kk,ll,mm,nn]=[-f[shovesb[2]], -g[shovesb[2]], -f[shovebb[3]], -g[shovebb[3]], -f[shovesb[3]], -g[shovesb[3]], -g[shovebb[4]], -g[shovesb[4]], -h[shovebb[5]], -g[shovebb[5]], -h[shovesb[5]], -g[shovesb[5]], -h[shovesb[6]], -g[shovesb[6]]]
+def yourface(shove):
+    [aa,bb,cc,dd,ee,ff,gg,hh,ii,jj,kk,ll,mm,nn]=[-f[shove[3]], -g[shove[3]], -f[shove[4]], -g[shove[4]], -f[shove[5]], -g[shove[5]], -g[shove[6]], -g[shove[7]], -h[shove[8]], -g[shove[8]], -h[shove[9]], -g[shove[9]], -h[shove[11]], -g[shove[11]]]
     bullshit=numpy.matrix([
     [1, 0, 0,-0.5, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 1,-0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -29,43 +29,41 @@ def yourface(shovesb, shovebb):
     [0, 0,kk, 0, 0, 0,ll, 0, 0, 1, 0, 0],
     [0, 0, 0, 0, 0, 0, 0,-0.5, 0, 0, 1, 0],
     [0, 0, 0, 0, 0, 0,mm, 0,nn,0, 0, 1]]) # magic
-    balloon = [0, 0, 0, 0, 0, 0, f[shovebb[4]], f[shovesb[4]], f[shovebb[5]], f[shovesb[5]], 0.5, f[shovesb[6]]]
+    balloon = [0, 0, 0, 0, 0, 0, f[shove[6]], f[shove[7]], f[shove[8]], f[shove[9]], 0.5, f[shove[11]]]
     return linalg.spsolve(bullshit,balloon) # basically this algorithm is a big Ax=b matrix calculation a bazillion times
 
 while True: # random walk
-    value = random.randint(1,8)
-    direction=random.randint(1,2)
-    walrus=shovesb[:]
-    octopus=shovebb[:]
-    if value<6:
-        if walrus[value+1]==168: # cheap method of getting rid of index errors
-            walrus[value+1]=167
-        elif direction==1:
-            walrus[value+1]+=1
-        else:
-            walrus[value+1]-=1
+    value = random.randint(3,10)
+    walrus=shove[:]
+    if value==10:
+        value+=1
+    if walrus[value]==168: # cheap method of getting rid of index errors
+        blobfish=True
+        walrus[value]=167
     else:
-        if octopus[value-3]==168:
-            octopus[value-3]=167
-        elif direction==1:
-            octopus[value-3]+=1
-        else:
-            octopus[value-3]-=1
-    cauliflower=yourface(walrus,octopus)
+        blobfish=False
+        walrus[value]+=1 # try making the range bigger
+    cauliflower=yourface(walrus)
     result=sum(w for w in cauliflower)
     if result>fish:
         fish = result
-        shovesb=walrus[:]
-        shovebb=octopus[:]
+        shove=walrus[:]
+    elif blobfish==False: # if making it bigger doesn't work, try making it smaller
+        walrus[value]-=2
+        cauliflower=yourface(walrus)
+        result=sum(w for w in cauliflower)
+        if result>fish:
+            fish = result
+            shove=walrus[:]
     if result>5.256723: # unique optimal. in this case it happens to be the same as the nash chart
-        print shovesb, shovebb
+        print shove
         fish = result
         print result
         print cauliflower
         break
 
 # the output should be
-# [168, 168, 168, 166, 155, 166, 168] [168, 168, 168, 168, 168, 168]
+# [168, 168, 168, 168, 168, 166, 168, 155, 168, 166, 168, 168]
 # 5.25672317619
 # [ 0.125       0.12559684  0.25119368  0.25        0.375       0.37569033
 #   0.5         0.50238736  0.625       0.62566128  0.75119368  0.75      ]
